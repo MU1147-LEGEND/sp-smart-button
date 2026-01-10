bcfe62bb96b5a076f8c51e9605a8b9628af3a2f5
Mohammad Ullah <ullahmohammad314@gmail.com>
2026-01-10 13:47:14 +0600

    feat: enhance button styles with transitions and update color schemes

--- build/sp-smart-buttons/index.asset.php
+++ build/sp-smart-buttons/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '46b87a1b88ebc8409bf3');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '1a878f257fbdbe0d957e');
